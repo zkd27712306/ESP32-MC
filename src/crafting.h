8726c561bcbd77b04125cc876dc2ad400a0646a4
zkd27712306 <2212239755@qq.com>
2026-07-28 18:49:43 +0800

    Delete src directory

--- src/crafting.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRAFTING_H
-#define CRAFTING_H
-
-#include "game_types.h"
-
-void getCraftingOutput(PlayerData *player, uint8_t *count, uint16_t *item);
-void getSmeltingOutput(PlayerData *player);
-
-#endif
